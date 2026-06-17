37029063c735f47caf649fa17538f67079c3c683
Backmann <backmannleo@gmail.com>
2026-06-17 16:45:45 +0200

    chore: remove stray patch scripts and ignore scratch files

--- app/run-planning.cjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-// Прогоняет оба патча режима «Планирование» по порядку.
-const { execSync } = require('child_process');
-for (const f of ['patch-planning-mode.cjs', 'patch-planning-locales.cjs']) {
-  console.log(`\n--- ${f} ---`);
-  execSync(`node ${f}`, { stdio: 'inherit' });
-}
