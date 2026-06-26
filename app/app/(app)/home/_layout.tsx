f1b575a20cc897cbcd4c7ff9b6397ec100385ea2
Backmann <backmannleo@gmail.com>
2026-06-26 11:01:58 +0200

    refactor(brand): move lockup into Home header, drop duplicate title

--- app/app/(app)/home/_layout.tsx
+++ app/app/(app)/home/_layout.tsx
@@ -1,11 +1,19 @@
 import { Stack } from 'expo-router';
 import { useTranslation } from 'react-i18next';
+import BrandLockup from '../../../components/BrandLockup';
 
 export default function HomeLayout() {
   const { t } = useTranslation();
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: t('home.title') }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: t('home.title'),
+          headerTitleAlign: 'left',
+          headerTitle: () => <BrandLockup mark={26} word={18} />,
+        }}
+      />
       <Stack.Screen
         name="my-assignments"
         options={{ title: t('home.myTasksScreen.title') }}
